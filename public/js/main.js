47e6216640de0c15cb0debf39cc5f96e1aab37fa
Eleni Kehrioti <elenikehrioti@gmail.com>
2023-12-29 17:10:22 +0200

    added script to show the categories

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,44 @@
-// Put your client side JS code here
-console.log('Hello world')
+window.onload = function() {
+    // This function will be called when the entire page has finished loading
+    var fetched_data;
+    const CATEGORY_TEMPLATE = Handlebars.compile(document.getElementById("categories-template").innerHTML);
+    const CATEGORY_CONTAINER = document.getElementById("categories-container");
+
+    function renderCategories() {
+        if (CATEGORY_CONTAINER) {
+            const html = CATEGORY_TEMPLATE(fetched_data);
+            CATEGORY_CONTAINER.innerHTML = html;
+            console.log(CATEGORY_CONTAINER.innerHTML);
+        } else {
+            console.error("Categories container not found.");
+        }
+    }
+
+    function list_categories() {
+        let url = "https://wiki-ads.onrender.com/categories";
+        let headers = new Headers();
+        headers.append('Accept', 'application/json');
+        let init = {
+            method: 'GET',
+            headers: headers
+        };
+        fetch(url, init)
+            .then(response => response.json())
+            .then(data => {
+                fetched_data = parseData(data);
+                renderCategories();
+            });
+    }
+
+    function parseData(data) {
+        var categories_arr = data.map(category => ({
+            id: category.id,
+            title: category.title,
+            img_url: ("https://wiki-ads.onrender.com/" + category.img_url)
+        }));
+
+        return { categories: categories_arr };
+    }
+
+    list_categories();
+};
