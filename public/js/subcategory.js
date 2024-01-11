8d84e39989b095b036ebae91dcd9a448215718e2
Eleni Kehrioti <elenikehrioti@gmail.com>
2024-01-11 16:24:55 +0200

    added slider for all listings

--- public/js/subcategory.js
+++ public/js/subcategory.js
@@ -2,6 +2,13 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 let id = urlParams.get('id');
+
+//const next = document.querySelector(".next");
+//const prev = document.querySelector(".prev");
+
+
+
+
 console.log(id);
 
 var fetched_data;
@@ -54,11 +61,42 @@ function parseAds(data){
         tmp.subcategory_id = ads.subcategory_id;
         tmp.description = ads.description;
         tmp.cost = ads.cost;
-        tmp.image = ads.images;
+        tmp.image = ads.images.map(image => "https://wiki-ads.onrender.com/" + image);
         tmp.firstimage = "https://wiki-ads.onrender.com/"+ads.images[0];
         tmp.features = ads.features ? ads.features.split(';') : [];
         ads_arr.push(tmp);
     }
     ads["ads"] = ads_arr;
     return ads;
+}
+
+Handlebars.registerHelper('addOne', function(index) {
+    return index + 1;
+});
+
+
+function showSlide(n, element) {
+    let slideNumber = n;
+    let slides = element.parentElement.querySelectorAll(".slide");
+    
+    if (slideNumber > slides.length) { slideNumber = 1 }
+    if (slideNumber < 1) { slideNumber = slides.length }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideNumber - 1].style.display = "block";
+}
+
+function nextSlide(element) {
+    let currentSlide = element.parentElement.querySelector(".slide[style='display: block;']");
+    let currentSlideNumber = parseInt(currentSlide.querySelector("img").dataset.slideNumber);console.log("Current Slide Number:", currentSlideNumber);
+    showSlide(currentSlideNumber + 1, element);
+}
+
+function prevSlide(element) {
+    let currentSlide = element.parentElement.querySelector(".slide[style='display: block;']");
+    let currentSlideNumber = parseInt(currentSlide.querySelector("img").dataset.slideNumber);console.log("Current Slide Number:", currentSlideNumber);
+    showSlide(currentSlideNumber - 1, element);
 }
