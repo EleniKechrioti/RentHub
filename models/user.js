2720575f08b3dfbd2a8edca27d5d044cd5c40123
Eleni Kehrioti <elenikehrioti@gmail.com>
2024-01-12 21:42:57 +0200

    changed to mongoose

--- models/user.js
+++ models/user.js
@@ -1,67 +1,26 @@
-// const mongoose = require('mongoose');
-// const Schema = mongoose.Schema;
-
-// const userSchema = new Schema({
-//     username: String,
-//     password: String,
-//     // cart:{
-//     //     cartItems:[
-//     //         {
-//     //             title:String,
-//     //             cost:Number,
-//     //             quantity:Number,
-//     //             image:String
-//     //         }
-//     //     ],
-//     //     totalcost:Number
-//     // }
 
 
-    
-// });
-
-// const user = mongoose.model('user',userSchema);
-
-
-// module.exports = user;
-
-
-exports.User = class {
-    /**
-     * Constructs an instance of the user class
-     * @param {string} username the user's name
-     * @param {string} password the user's password
-     */
-    constructor(username, password){
-        this.username=username;
-        this.password=password;
-        this.sessionId=undefined;
-        this.favorites=[];
+  const mongoose = require('mongoose');
+  const Schema = mongoose.Schema;
+  
+  const userSchema = new Schema({
+    username: String,
+    password: String,
+    favorites:{
+        Object:[
+            {
+                id:Number,
+                description:String,
+                title:String,
+                cost:Number,
+                image:String
+            }
+        ]
     }
-
-    /**
-     * gets a user's cart size
-     * @returns a JS object that contains the user's cart size
-     */
-    getFavoritesSize(){
-        return {'size': this.favorites.length};
-    }
-
-    /**
-     * Empties the user's favorites list, called by the server every time a new sessionId is assigned to a user
-     */
-    emptyFavorites(){
-        this.favorites=[];
-    }
-
-    /**
-     * Adds item to user's favorites
-     * @param {Object} listing the listing to be added to the favorites
-     */
-    addTofavorites(listing){
-        const index = this.favorites.findIndex(object => object.product.id === listing.product.id);
-        if(index === -1){
-            this.favorites.push(item);
-        }
-    }
-}
+}); 
+  
+  
+  const user = mongoose.model('contacts',userSchema);
+  
+  
+  module.exports = user;
